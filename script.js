07e5875b9bad6b780babe3afb4846ca42cb42187
Ornella Facciola <102764364+ornefacciola@users.noreply.github.com>
2022-06-23 11:58:43 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,16 +1,57 @@
-const name = document.getElementById('name')
-const tel = document.getElementById('tel')
-const elegi = document.getElementById('text')
-const form = document.getElementById('form')
+const form = document.querySelector('form');
+const validName = document.querySelector('input[name="name"]');
+const validTel = document.querySelector('input[name="tel"]');
+
+
+
+const inputs = [validName, validTel]; 
+
+  
+let isFormValid = false; 
+let isValidationOn= false;
+
+const resetElm = (elm) => { //si es valido se sale la alerta de error 
+    elm.nextElementSibling.classList.add("hidden");
+}
+
+const invalidateElm = (elm) => { //si es invalido mostramos el mensaje 
+    elm.nextElementSibling.classList.remove("hidden"); 
+}
+const validateInputs = () => { //checkeamos que
+    if (!isValidationOn) return; //if it is false 
+    
+    isFormValid = true; //now it is true and we are going to check the info 
+    inputs.forEach(resetElm); 
+    
+    if (!validName.value) {  //if the email is empty 
+        isFormValid=false; //the validation is not correct
+        invalidateElm(validName); //we show that it is not valid. 
+    }
+    
+    if (!validTel.value) {
+        isFormValid=false;
+        invalidateElm(validTel);
+    }
+}
+
+
+inputs.forEach(input => { //everytime an input is clicked we must start validating them 
+    input.addEventListener("input", () => {
+        validateInputs();
+    })
+})
+
 
 form.addEventListener('submit', (e) => {
-    let messages = []
-    if (name.value === '' || name.value == null) {
-        messages.push('Ingrese su Nombre')
-    }
+    e.preventDefault();
+    isValidationOn = true; //the validation must be true to submit 
+    validateInputs();
+    if (isFormValid) { 
+        window.location.replace("./thankyou.html")
 
-    if(messages.length > 0) {
-        e.preventDefault()
-        errorElement.innerText = messages.join(', ')
     }
-})
+});
+
+validEmail.addEventListener("input", () => {
+    validateInputs();
+});
